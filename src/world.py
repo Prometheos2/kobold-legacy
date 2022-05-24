706360a481126ed9d2f308e22df13131fa90302d
Fork my Git <45640841+Prometheos2@users.noreply.github.com>
2022-05-25 00:52:58 +0200

    Extract Kobold

--- src/world.py
+++ src/world.py
@@ -2,10 +2,11 @@ import math
 import random
 import time
 
+from kobold import Kobold
 from tile import Tile
 from tribe import Tribe
 
-from ..kobold import (Kobold, chance, choice, console_print,
+from ..kobold import (chance, choice, console_print,
                       find_building, forage, game_print, get_tri_distance,
                       item_cats, item_data, landmark_data, liquid_data,
                       skill_data, spawn_item, trait_data)
